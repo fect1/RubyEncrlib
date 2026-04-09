31e8004fd6d182808517541510a62a3d7f71b660
brainfeveralltime <nubasick456@gmail.com>
2026-04-09 14:22:56 +0300

    fix tests

--- test/test_atbash_cipher.rb
+++ test/test_atbash_cipher.rb
@@ -4,43 +4,43 @@ require_relative '../lib/encrlib/atbash'
 class AtbashCipherTest < Minitest::Test
   def test_atbash_encrypts_english_text_upper
     original = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 
   def test_atbash_encrypts_english_text_lower
     original = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 
   def test_atbash_encrypts_russian_text_upper
     original = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 
   def test_atbash_encrypts_russian_text_lower
     original = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 
   def test_atbash_encrypts_mixed_text
     original = 'abcdEFGHзийкЛМНО'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 
   def test_atbash_keeps_other_characters
     original = '12345 !@#$'
-    encrypted = atbash_cipher(original)
-    decrypted = atbash_cipher(encrypted)
+    encrypted = Encrlib::Atbash.atbash_cipher(original)
+    decrypted = Encrlib::Atbash.atbash_cipher(encrypted)
     assert_equal original, decrypted
   end
 end
