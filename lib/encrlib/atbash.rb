e0dd1fc8ba7e32139a48bc4d030fa21b2e41934e
brainfeveralltime <nubasick456@gmail.com>
2026-04-09 14:20:34 +0300

    add class atbash

--- lib/encrlib/atbash.rb
+++ lib/encrlib/atbash.rb
@@ -1,27 +1,35 @@
 # frozen_string_literal: true
 
-# string encryption with Atbash cipher|шифрование строки шифром Атбаш
-# @param str [String] input string|исходная строка
-# @return [String] encrypted string|зашифрованная строка
-def atbash_cipher(str)
-  upper_rus = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-  lower_rus = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-  upper_eng = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  lower_eng = 'abcdefghijklmnopqrstuvwxyz'
+module Encrlib
+  class Atbash
+    class << self
+      # @param [String] sym
+      def atbash_symbch(sym)
+        upper_rus = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+        lower_rus = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        upper_eng = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        lower_eng = 'abcdefghijklmnopqrstuvwxyz'
 
-  encstr = ''
-  str.each_char do |ch|
-    if (idx = upper_rus.index(ch))
-      encstr += upper_rus[-1 - idx]
-    elsif (idx = lower_rus.index(ch))
-      encstr += lower_rus[-1 - idx]
-    elsif (idx = upper_eng.index(ch))
-      encstr += upper_eng[-1 - idx]
-    elsif (idx = lower_eng.index(ch))
-      encstr += lower_eng[-1 - idx]
-    else
-      encstr += ch
+        if (idx = upper_rus.index(sym))
+          upper_rus[-1 - idx]
+        elsif (idx = lower_rus.index(sym))
+          lower_rus[-1 - idx]
+        elsif (idx = upper_eng.index(sym))
+          upper_eng[-1 - idx]
+        elsif (idx = lower_eng.index(sym))
+          lower_eng[-1 - idx]
+        else
+          sym
+        end
+      end
+
+      # string encryption with Atbash cipher|шифрование строки шифром Атбаш
+      # @param str [String] input string|исходная строка
+      # @return [String] encrypted string|зашифрованная строка
+      def atbash_cipher(str)
+        str.each_char.map { |ch| atbash_symbch(ch) }.join
+      end
+
     end
   end
-  encstr
 end
